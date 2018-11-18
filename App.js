121f5690fb24adae8b47617fdbc750ca20a2be35
peterayeniofficial <ayenicology@gmail.com>
2018-11-18 13:55:21 +0100

    Add screen boilerplate

--- App.js
+++ App.js
@@ -1,21 +1,112 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, StatusBar, Platform } from "react-native";
+import { TabNavigator, StackNavigator } from "react-navigation";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Constants } from "expo";
+
+// utils
+import { purple } from "./utils/colors";
+
+// Screens Import
+import AddCard from "./screens/AddCard";
+import AddDeck from "./screens/AddDeck";
+import Deck from "./screens/Deck";
+import Decks from "./screens/Decks";
+import Quiz from "./screens/Quiz";
+import { white } from "ansi-colors";
+
+function FlashStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
+
+const Tabs = TabNavigator(
+  {
+    Decks: {
+      screen: Decks,
+      navigationOptions: {
+        tabBarLabel: "Decks",
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="align-justify" size={30} color={tintColor} />
+        )
+      }
+    },
+    AddDeck: {
+      screen: AddDeck,
+      navigationOptions: {
+        tabBarLabel: "Add Deck",
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="plus-square" size={30} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    navigationOptions: {
+      header: null
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Platform.OS === "ios" ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === "ios" ? white : purple,
+        shadowRadius: 6,
+        shadowOpacity: 1,
+        shadowColor: "rgba(0,0,0,0.24)",
+        shadowOffset: {
+          width: 0,
+          height: 3
+        }
+      }
+    }
+  }
+);
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs
+  },
+  AddCard: {
+    screen: AddCard,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
+  },
+  Deck: {
+    screen: Deck,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
+  },
+  Quiz: {
+    screen: Quiz,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
+  }
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flex: 1 }}>
+        <FlashStatusBar backgroundColor={purple} barStyle="light-content" />
+        <MainNavigator />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
